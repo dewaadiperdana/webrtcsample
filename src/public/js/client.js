404b75f137599847700a6ff86eabc01b85fe328f
Adi <dewaadiperdana@gmail.com>
2019-09-29 11:17:59 +0800

    Change iceserver configuration v2

--- src/public/js/client.js
+++ src/public/js/client.js
@@ -16,14 +16,9 @@ var iceServers = {
       urls: "stun:stun.l.google.com:19302"
     },
     {
-      urls: "turn:192.158.29.39:3478?transport=udp",
-      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-      username: "28224511:1379330808"
-    },
-    {
-      urls: "turn:192.158.29.39:3478?transport=tcp",
-      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-      username: "28224511:1379330808"
+      urls: ["turn:13.250.13.83:3478?transport=udp"],
+      username: "YzYNCouZM1mhqhmseWk6",
+      credential: "YzYNCouZM1mhqhmseWk6"
     }
   ]
 };
